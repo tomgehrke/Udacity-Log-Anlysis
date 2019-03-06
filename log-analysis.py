28f598bccf7f7ab153073c4f29488618f5c5a4de
Thomas Gehrke <tomgehrke@gmail.com>
2019-03-06 15:02:02 -0600

    Added code comments

--- log-analysis.py
+++ log-analysis.py
@@ -4,21 +4,29 @@ import psycopg2
 import psycopg2.extras
 import os
 
+# DATABASE-RELATED
 DATABASE_NAME = "news"
 TOP3ARTICLES_VIEW = "v_top3articles"
+TOPAUTHORS_VIEW = "v_topauthors"
+TOPERRORDAYS_VIEW = "v_toperrordays"
+
+# REPORTING
 TOP3ARTICLES_HEAD = "What are the most popular three articles of all time?"
 TOP3ARTICLES_ROW = " * \"{title}\" -- {count} views"
-TOPAUTHORS_VIEW = "v_topauthors"
 TOPAUTHORS_HEAD = "Who are the most popular authors of all time?"
 TOPAUTHORS_ROW = " * {author} -- {count} views"
-TOPERRORDAYS_VIEW = "v_toperrordays"
 TOPERRORDAYS_HEAD = "On which days did more than 1% of requests " \
     "lead to errors?"
 TOPERRORDAYS_ROW = " * {day:%B %d, %Y} -- {error_rate:.1f}% errors"
+
+# APPLICATION MESSAGES
 CHECKING_VIEWS = "Checking for supporting views..."
-VIEW_EXISTS = "+ View '{0}' exists!"
 VIEW_DOES_NOT_EXIST = "- View '{0}' does NOT exist! Creating it..."
+VIEW_ISSUE = "There are problems with the supporting views!"
 CREATE_VIEW_EXCEPTION = "- Unable to create view {0}!"
+RUN_COMPLETE = "[END]"
+
+# SQL
 SELECT_VIEW = '''
     SELECT table_name
     FROM information_schema.views
@@ -75,6 +83,7 @@ CREATE_TOPERRORDAYS_VIEW = '''
     '''
 
 
+# Checks for the existence of supporting views
 def views_exist():
     return (
         check_view(TOP3ARTICLES_VIEW)
@@ -83,18 +92,25 @@ def views_exist():
     )
 
 
+# Checks for the existence of a particular view and creates it if
+# necessary. If the view exists or was successfully created, the
+# function returns True.
 def check_view(name):
     view_exists = True
     news_connection = psycopg2.connect(database=DATABASE_NAME)
     news_cursor = news_connection.cursor()
+    # Looks for the view by querying the database's information_schema
     news_cursor.execute(SELECT_VIEW.format(name))
     news_cursor.fetchall()
 
+    # As long as we got something back, we should be good.
     if news_cursor.rowcount > 0:
         return view_exists
     else:
+        # The view does not exist (probably because it is the app's first run).
         print(VIEW_DOES_NOT_EXIST.format(name), end=" ")
 
+        # Let's try to create that view!
         try:
             if name == TOP3ARTICLES_VIEW:
                 news_cursor.execute(CREATE_TOP3ARTICLES_VIEW.format(name))
@@ -112,14 +128,17 @@ def check_view(name):
             if view_exists:
                 print("SUCCESS!\n")
             news_connection.close()
-
         return view_exists
 
 
 def get_top3articles():
+    # Function provides list of the three most popular articles.
     print(TOP3ARTICLES_HEAD + "\n")
     news_connection = psycopg2.connect(database=DATABASE_NAME)
-    news_cursor = news_connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    # Return a dictionary cursor to allow referencing collumns by name
+    # instead of by index.
+    news_cursor = news_connection.cursor(
+        cursor_factory=psycopg2.extras.DictCursor)
     news_cursor.execute("SELECT * FROM {0}".format(TOP3ARTICLES_VIEW))
     records = news_cursor.fetchall()
     for record in records:
@@ -127,14 +146,18 @@ def get_top3articles():
             title=record["title"],
             count=record["article_count"])
             )
-    print("\n\n")
+    print("\n")
     news_connection.close
 
 
 def get_topauthors():
+    # Function provides list of authors sorted by popularity.
     print(TOPAUTHORS_HEAD + "\n")
     news_connection = psycopg2.connect(database=DATABASE_NAME)
-    news_cursor = news_connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    # Return a dictionary cursor to allow referencing collumns by name
+    # instead of by index.
+    news_cursor = news_connection.cursor(
+        cursor_factory=psycopg2.extras.DictCursor)
     news_cursor.execute("SELECT * FROM {0}".format(TOPAUTHORS_VIEW))
     records = news_cursor.fetchall()
     for record in records:
@@ -142,14 +165,18 @@ def get_topauthors():
             author=record["name"],
             count=record["article_count"])
             )
-    print("\n\n")
+    print("\n")
     news_connection.close
 
 
 def get_toperrordays():
+    # Function provides answer to which days were the most error-prone.
     print(TOPERRORDAYS_HEAD + "\n")
     news_connection = psycopg2.connect(database=DATABASE_NAME)
-    news_cursor = news_connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    # Return a dictionary cursor to allow referencing collumns by name
+    # instead of by index.
+    news_cursor = news_connection.cursor(
+        cursor_factory=psycopg2.extras.DictCursor)
     news_cursor.execute("SELECT * FROM {0}".format(TOPERRORDAYS_VIEW))
     records = news_cursor.fetchall()
     for record in records:
@@ -157,15 +184,16 @@ def get_toperrordays():
             day=record["log_date"],
             error_rate=record["error_rate"])
             )
-    print("\n\n")
+    print("\n")
     news_connection.close
 
 
 os.system('clear')
 if views_exist() is not True:
-    print("There are problems with the supporting views!")
+    # The supporting views don't exist so no point in continuing.
+    print(VIEW_ISSUE)
 else:
     get_top3articles()
     get_topauthors()
     get_toperrordays()
-    print("[END]")
+    print(RUN_COMPLETE)
